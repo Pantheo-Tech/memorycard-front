0f8ac5719f3be3399a1db7edb11a95f6a4c8e112
flaviare1s <flaviarster@gmail.com>
2025-01-31 08:42:07 -0300

    fix: adjust background image to cover full screen

--- src/components/landing-page/Background.tsx
+++ src/components/landing-page/Background.tsx
@@ -2,10 +2,12 @@ import background from "../../assets/img/background-landing-page.png";
 
 const Background = () => {
   return (
-    <div>
-      <div className="absolute top-0 left-0 w-full h-full object-cover -z-10">
-        <img src={background} alt="background" />
-      </div>
+    <div className="absolute w-full h-screen">
+      <img
+        src={background}
+        alt="background"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      />
       <div className="bg-black opacity-60 absolute top-0 left-0 w-full h-full"></div>
     </div>
   );
